fc0b2eb9ddf5bc3106c195f19540e385309f0b82
iyshmn <bestinworld013@outlook.com>
2020-12-16 20:14:32 +0530

    updated

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,5 +2,6 @@ package app;
 public class App{
     public static void main(String[] args){
         System.out.println("Hello World app");
+        System.out.println("We are leaning version controles");
     }
 }
